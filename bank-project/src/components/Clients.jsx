43f94c60074fdc345dddd2b983d8ccfd40ddeca9
nuwagalb <isnuwa@gmail.com>
2024-11-29 16:44:38 +0300

    add client section id attribute

--- bank-project/src/components/Clients.jsx
+++ bank-project/src/components/Clients.jsx
@@ -2,7 +2,7 @@ import { clients } from "../constants";
 import styles from "../style";
 
 const Clients = () => (
-  <section className={`${styles.flexCenter} my-4`}>
+  <section id='clients' className={`${styles.flexCenter} my-4`}>
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
       {clients.map((client) => (
         <div className={`flex-1 ${styles.flexCenter} sm:min-w-[192px]
